b1b3449cac8fd87ae6c2603bbf22681e3fe1b7e0
Duncan Temple Lang <duncan@r-project.org>
2024-01-27 13:54:23 -0800

    text/regex processing to get the parts of a MySQL export as SQL. Specific to a particular database so there will be other pieces in other DBs. Coverse functions and procedures, create table, alter table, insert into

---
+++ R/parseMYSQLExport.R
@@ -0,0 +1,44 @@
+mysqlExport =
+function(sql, asText = !file.exists(sql))
+{
+    if(!asText)
+        sql = readLines(sql)
+
+    s = grep("^CREATE (PROCEDURE|FUNCTION)", sql)
+    e = grep("^ *END\\$\\$", sql)
+
+    procFuns = mkProcFuns(sql, s, e)
+    sql = d[ - unlist(mapply(seq, s, e)) ]
+
+    sql = removeComments(sql)
+    d4 = strsplit(gsub("\\n\\n+", "\n\n", paste(sql, collapse = "\n")), "\\n")[[1]]
+    g = split(d4, cumsum(d4 == ""))
+    g = lapply(g, function(x) if(x[1] == "") x[-1] else x)
+    names(g)
+    names(g) = gsub(" .*", "", sapply(g, `[`, 1))
+
+    g2 = split(g, names(g))
+    g2$procFuns = procFuns
+
+    g2
+}
+
+mkProcFuns =
+function(x, s, e)
+{
+    procFuns = mapply(function(s, e) d[seq(s, e)], s, e)
+    names(procFuns) = gsub("[^`]*`([^`]+)`.*", "\\1", sapply(procFuns, `[`, 1))
+    procFuns
+}
+
+
+removeComments =
+function(x)
+    x[!isCommentLine(x)]
+
+
+isCommentLine =
+function(x)
+    grepl("^--", x) |  grepl("^ */\\*.*\\*/;?$", x)
+
+
